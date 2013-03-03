a95566052bb8d6ab22b42a7dcdad6528a0ea31d3
Udi Wertheimer <udi.wertheimer@gmail.com>
2013-03-03 15:03:10 +0200

    Add tests for error messages and flash notice

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,6 +30,14 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('errors') }
+        it { should have_selector('div.field_with_errors') }
+      end
     end
 
     describe "with valid information" do
@@ -50,6 +58,7 @@ describe "User pages" do
         let(:user) { User.find_by_email("user@example.com") }
 
         it { should have_selector('title'), text: user.name }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
     
